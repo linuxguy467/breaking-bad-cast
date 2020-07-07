c173f31c9037376cc92875a31d67f0072040e629
Matthew Hemingway <dpplrdr435@gmail.com>
2020-07-07 14:58:54 -0400

    Added hooks to App component to manage state and get data from api. Added CharacterGrid component to contain the character cards into a grid layout. Added CharacterItem component to display character info from api.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
